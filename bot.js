637e20dd40af24065ec33d1a5e056cbccc39055f
yagizsogut2014-lgtm <yagizsogut2014@gmail.com>
2026-09-19 14:39:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,17 +3,17 @@ const mineflayer = require('mineflayer');
 const bot = mineflayer.createBot({
   host: 'sofiasky.mcsh.io', // Buraya MC sunucu IP'nizi yazın
   port: 25565,                 // Port (Genelde 25565'tir)
-  username: 'ChatBotYardimcisi' // Botun oyundaki adı
+  username: 'ChatBotYardimcisi', // Botun oyundaki adı
+  version: '1.21.4'            // Kesin olarak 1.21.4 belirtiyoruz
 });
 
 bot.on('spawn', () => {
-  console.log('Bot başarıyla oyuna girdi!');
+  console.log('Bot başarıyla 1.21.4 sunucusuna girdi!');
 });
 
 bot.on('chat', (username, message) => {
   if (username === bot.username) return;
 
-  // Örnek tetikleyici
   if (message.toLowerCase().includes('kutular nerede')) {
     setTimeout(() => {
       bot.chat(`Kutular /warp crates konumunda, @${username}!`);
